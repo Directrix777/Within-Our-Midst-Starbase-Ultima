c32cdf3fcf6bdf79353028b0dbfe4bec7b79e885
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-27 14:50:42 -0500

    Added some controller actions to user

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def create
+        user = User.new
+        user.name = params[:name]
+        user.color = params[:color]
+        user.save
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        if user
+            user.delete
+            render json: {message: 'Successfully released!'}
+        else
+            render json: {message: 'User not found'}
+        end
+    end
 end
