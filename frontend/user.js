d7ffbf3fcd7c5fdffcf0d6b0fa6b06243629ceea
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-28 11:34:57 -0500

    downloaded bootstrap

--- frontend/user.js
+++ frontend/user.js
@@ -4,6 +4,23 @@ class User
     {
         this.name = name
         this.alive = true
-        //this.color = 
+        this.dbVersion = this.makeDbVersion()
     }
+
+    makeDbVersion()
+    {
+        let options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({name: this.name})
+        }
+        return fetch('http://localhost:3000/users/', options)
+        .then(r => r.json)
+        .then(r => console.log(r))
+    }
+
+
 }
