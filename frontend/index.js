879b3931458a34200d48c139562a3241cbc8f1b1
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-30 13:32:59 -0500

    renamed keyset, adjested keys to match

--- frontend/index.js
+++ frontend/index.js
@@ -2,22 +2,18 @@ const pubID = parseInt(Math.random()*1000000)
 console.log(pubID)
 const pubnub = new PubNub({
     uuid: pubID,
+    publish_key: 'pub-c-a2f1cf1d-ce28-4242-8e93-cf10ca8cbafe',
+    subscribe_key: 'sub-c-44658cae-17a8-11eb-bc34-ce6fd967af95',
+    ssl: true
+});
+const tasks = new PubNub({
+    uuid: pubID,
     publish_key: 'pub-c-b1e91afd-fb2e-4e9c-b199-907f7512cbad',
     subscribe_key: 'sub-c-691174e4-17aa-11eb-a3e5-1aef4acbe547',
     ssl: true
-});
+})
 const user = new User(prompt("Enter your player name"), pubID)
 pubnub.subscribe({channels: ["aliveChat"], withPresence: true});
-pubnub.hereNow(
-    {
-      channels: ["aliveChat"],
-      includeState: true
-    },
-    function (status, response) {
-      console.log(status, response);
-    }
-    
-);
 let leave = document.createElement('button')
 leave.addEventListener('click', function(){user.breakDbVersion()})
 leave.innerText = "Leave"
@@ -60,6 +56,14 @@ pubnub.addListener({
         if(p.action == 'join')
         {
             console.log("Someone joined!")
+            pubnub.hereNow(
+                {
+                  channels: ["aliveChat"],
+                  includeState: true
+                },
+                function (status, response) {
+                  console.log(status, response);
+                })
         }
 
         let action = p.action; // Can be join, leave, state-change, or timeout
