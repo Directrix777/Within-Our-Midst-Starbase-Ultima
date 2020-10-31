ba3ed0206352a3d147ce8f2baba15227559c6bf1
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-30 22:52:59 -0500

    Created tasks

---
+++ frontend/task.js
@@ -0,0 +1,33 @@
+class Task
+{
+    constructor(userId)
+    {
+        this.userId = userId
+        this.name = this.randomTask()
+    }
+
+    randomTask()
+    {
+        const taskNames = ['Dropship: Chart Course', 'Courtyard: Monitor Tree', 'O2: Clean O2 Filter', 'Greenhouse: Clean O2 Filter', 'Laboratory: Record Temperature', 'Reactor: Unlock Manifolds', 'Storage: Store Artifacts', 'Laboratory: Assemble Artifact', 'Cafeteria: Buy Beverage', 'Navigation: Prime Shields', 'Navigation: Stabilize Steering', 'Navigation: Chart Course', 'O2: Fill Canisters', 'Navigation: Clear Asteroids', 'Hangar: Run Diagnostics']
+        const num = parseInt(Math.random() * 15)
+        console.log(num)
+        return taskNames[num]
+    }
+
+    makeDbVersion()
+    {
+        let options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({task_name: this.name, user_id: this.userId})
+        }
+        // fetch('http://localhost:3000/tasks', options)
+        // .then(r => r.json())
+        // .then(r => { 
+        //     console.log(r)
+        // })
+    }
+}
