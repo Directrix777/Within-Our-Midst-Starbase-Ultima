216ddbccd2c05137978a9d1efe4bec3681157eb6
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-30 22:53:16 -0500

    added create action

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -1,2 +1,9 @@
 class TasksController < ApplicationController
+    def create
+        task = Task.new
+        task.user_id = params[:user_id]
+        task.task_name = params[:task_name]
+        task.save
+        render json: params
+    end
 end
